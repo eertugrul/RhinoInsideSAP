130ffaf026b998654a43bb9475a9c87da6ba7ae2
Elcin <elcinertugrul@thorntontomasetti.com>
2018-10-27 17:15:18 -0400

    rhino inside etabs simple sphere working

---
+++ RhinoInside.ETABS.Sphere/Test1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+using Rhino.Geometry;
+using System.Reflection;
+
+using System.IO;
+using Rhino.Runtime.InProcess;
+using System.Diagnostics;
+using ETABSv17;
+
+namespace RhinoInside.ETABS.Sphere
+{
+    public class Test1
+    {
+        static Test1()
+        {
+            ResolveEventHandler OnRhinoCommonResolve = null;
+            AppDomain.CurrentDomain.AssemblyResolve += OnRhinoCommonResolve = (sender, args) =>
+            {
+                const string rhinoCommonAssemblyName = "RhinoCommon";
+                var assemblyName = new AssemblyName(args.Name).Name;
+
+                if (assemblyName != rhinoCommonAssemblyName)
+                    return null;
+
+                AppDomain.CurrentDomain.AssemblyResolve -= OnRhinoCommonResolve;
+
+                //string rhinoSystemDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "Rhino WIP", "System");
+                string rhinoSystemDir = @"C:\Users\EErtugrul\Desktop\Rhino WIP\System";
+                return Assembly.LoadFrom(Path.Combine(rhinoSystemDir, rhinoCommonAssemblyName + ".dll"));
+            };
+        }
+
+        private static RhinoCore rhinoCore;
+
+        public void Main(ref cSapModel Model)
+        {
+            if (rhinoCore == null)
+            {             // Load Rhino
+                try
+                {
+                    var schemeName = "ETABS";
+                    rhinoCore = new RhinoCore(new string[] { $"/scheme={schemeName}", "/nosplash" }, WindowStyle.Normal, Process.GetCurrentProcess().MainWindowHandle);
+                }
+                catch (Exception e)
+                {
+                    Debug.Fail(e.Source, e.Message);
+                    return;
+                }
+            }
+
+
+            // RhinoCommon code
+            var sphere = new Rhino.Geometry.Sphere(Point3d.Origin, 12000);
+            var brep = sphere.ToBrep();
+            var mp = MeshingParameters.Default;
+            var meshes = Rhino.Geometry.Mesh.CreateFromBrep(brep, mp);
+
+            foreach (var mesh in meshes.ToList())
+            {
+                for (int i = 0; i < mesh.Faces.Count; i++)
+                {
+                    Point3f a;
+                    Point3f b;
+                    Point3f c;
+                    Point3f d;
+
+                    mesh.Faces.GetFaceVertices(i, out a, out b, out c, out d);
+
+                    List<Point3f> vertices = new List<Point3f>();
+                    vertices.Add(a); vertices.Add(b); vertices.Add(c);
+                    if (c != d) vertices.Add(d);
+
+                    List<string> points = new List<string>();
+
+                    foreach (var v in vertices)
+                    {
+                        string p = string.Empty;
+                        Model.PointObj.AddCartesian(v.X, v.Y, v.Z, ref p);
+                        points.Add(p);
+                    }
+
+                    string area = string.Empty;
+                    string[] pts = points.ToArray();
+                    Model.AreaObj.AddByPoint(points.Count, ref pts,ref area);
+                    
+                }
+
+            }
+
+
+        }
+
+
+
+
+    }
+}
--- RhinoInside.ETABS.Sphere/cPlugin.cs
+++ RhinoInside.ETABS.Sphere/cPlugin.cs
@@ -1,21 +1,21 @@
-﻿using System;
+﻿using ETABSv17;
+using Rhino.Geometry;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
-using SAP2000v20;
 using System.Windows.Forms;
-using Rhino.Geometry;
-using System.Reflection;
-using System.IO;
-using Rhino.Runtime.InProcess;
 
-namespace RhinoInside.SAP.Sphere
+namespace RhinoInside.ETABS.Sphere
 {
     [ClassInterface(ClassInterfaceType.AutoDual)]
     public class cPlugin
     {
+
         /// <summary>k
         /// plug-in data
         /// </summary>
@@ -23,11 +23,37 @@ namespace RhinoInside.SAP.Sphere
         /// <param name="Isapplugin"></param>
         public void Main(ref cSapModel Model, ref cPluginCallback IsapPlugin)
         {
+
             try
             {
+                //// do something
+                //MessageBox.Show("hello world");
+                
+
 
                 var t = new Test1();
-                t.Main();
+                t.Main(ref Model);
+
+                //// RhinoCommon code
+                //var sphere = new Rhino.Geometry.Sphere(Point3d.Origin, 12000);
+                //var brep = sphere.ToBrep();
+                //var mp = MeshingParameters.Default;
+                //var meshes = Rhino.Geometry.Mesh.CreateFromBrep(brep, mp);
+
+                //foreach (var mesh in meshes.ToList())
+                //{
+                //    for (int i = 0; i < mesh.Faces.Count; i++)
+                //    {
+                //        Point3f a;
+                //        Point3f b;
+                //        Point3f c;
+                //        Point3f d;
+
+                //        mesh.Faces.GetFaceVertices(i, out a, out b, out c, out d);
+                //    }
+
+                //}
+
 
             }
             catch (Exception e)
@@ -48,6 +74,5 @@ namespace RhinoInside.SAP.Sphere
             info = "Rhino Inside SAP simple sphere plug-in \r\n Author : AEC Tech";
             return 0;
         }
-
     }
 }
