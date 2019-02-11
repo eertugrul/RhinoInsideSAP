84ad943fb458cbb58927b40f40b43ea7c06ba83b
Elcin <eertugrul@thorntontomasetti.com>
2019-02-11 09:24:47 -0500

    SAP plugin registration bat files

---
+++ registration/UnRegisterRhinoInsideSAP- DEBUGy.bat
@@ -0,0 +1,21 @@
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
+echo.
+echo   Install stuff remotely
+echo.
+echo   Note:  Machines that are unplugged, not connected to the network,
+echo          or on a different subnet will not play ball
+echo          This will update or install TTX 05 SAP
+echo          When run as administrator from a remote machine
+echo.
+:RETRY
+
+
+SET regasmpath="C:\Windows\Microsoft.NET\Framework\v4.0.30319"
+SET dllpath="C:\Users\EErtugrul\Documents\GitHub\RhinoInsideSAP\RhinoInside.SAP\RhinoInside.SAP.Sphere\bin\Debug"
+%regasmpath%\regasm /tlb /u %dllpath%\RhinoInside.SAP.Sphere.dll
+
+
+:END
+endlocal
+PAUSE
